d0e93e18c56327216489e6424b58d57030d463f5
Pedro Rocha <120132401@aluno.unb.br>
2018-03-22 23:45:57 -0300

    Adicionar Src
    Arquivos com as implementações dos métodos das classes necessárias para o projeto.

---
+++ src/Music.cpp
@@ -0,0 +1,40 @@
+#include "Music.h"
+
+Music::Music() {
+	music = nullptr;
+}
+
+Music::Music(std::string arq) {
+	music = nullptr;
+}
+void Music::Play(int times=-1) {
+	if (music != nullptr) {
+		int g = Mix_FadeInMusic(music, times,100);
+		if (g != 0) {
+			printf("Mix_PlayMusic: %s\n", Mix_GetError());
+		}
+	}
+	else {
+		printf("Erro no Mix_PlayMusic:  %s.\n", Mix_GetError());
+		getchar();
+		exit(1);
+	}
+}
+void Music::Stop(int msToStop=1500) {
+	Mix_FadeOutMusic(msToStop);
+}
+void Music::Open(std::string arq) {
+	music = Mix_LoadMUS(arq.c_str());
+	if (music == nullptr) {
+		printf("Erro no Mix_LoadMUS:  %s.\n", Mix_GetError());
+		getchar();
+		exit(1);
+	}
+}
+bool Music::IsOpen() {
+	return music != nullptr;
+}
+Music::~Music() {
+	Stop();
+	Mix_FreeMusic(music);
+}
